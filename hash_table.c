1250a87eaefb7a8d02415d0db31cd007b827c1c3
mortenl-dev <mortenlins@gmail.com>
2024-02-16 05:13:23 +0100

    first commit

---
+++ hash_table.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+static ht_item* ht_new_item(const char* k, const char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
+    i->key = strdup(k); //copies string
+    i->value = strdup(v); //->assigns the value in the variable (right) to the pointer (left)
+    return i;
+}
+
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*)); //allocates "empty" memory
+    return ht;
+}
