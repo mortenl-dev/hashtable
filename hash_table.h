75e3a8303898e3f685d00afe33fdf8e7de28103a
mortenl-dev <mortenlins@gmail.com>
2024-02-18 03:57:35 +0100

    Implemented hashing, collisions, API methods and resizing

--- hash_table.h
+++ hash_table.h
@@ -8,3 +8,6 @@ typedef struct {
     int count;
     ht_item** items;
 } ht_hash_table;
+
+const int HT_PRIME_1 = 151;
+const int HT_PRIME_2 = 163;
---
+++ main.c
@@ -0,0 +1,144 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+#pragma region methods
+static ht_item* ht_new_item(const char* k, const char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
+    i->key = strdup(k); //copies string
+    i->value = strdup(v); //->assigns the value in the variable (right) to the pointer (left)
+    return i;
+}
+
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*)); //allocates "empty" memory
+    return ht;
+}
+
+static void ht_del_item(ht_item* i) {
+    free(i->key);
+    free(i->value);
+    free(i);
+}
+
+void ht_del_hash_table(ht_hash_table* ht) {
+    for (int i = 0; i < ht->size; i++) {
+        ht_item* item = ht->items[i];
+        if (item != NULL) {
+            ht_del_item(item);
+        }
+    }
+    free(ht->items);
+    free(ht);
+}
+
+static int ht_hash(const char* s, const int a, const int m) {
+    long hash = 0;
+    const int len_s = strlen(s);
+    for (int i = 0; i < len_s; i++) {
+        hash += (long)pow(a, len_s - (i+1)) * s[i];
+        hash = hash % m;
+    }
+    return (int)hash;
+}
+
+
+
+static int ht_get_hash(
+    const char* s, const int num_buckets, const int attempt
+) {
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+#pragma endregion
+
+#pragma region API
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
+    ht_item* item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item* cur_item = ht->items[index];
+    int i = 1;
+    while (cur_item != NULL && cur_item != &HT_DELETED_ITEM) {
+        if (strcmp(cur_item->key, key) == 0) { //update value by deleting previous bucket
+                ht_del_item(cur_item);
+                ht->items[index] = item;
+                return;
+            }
+        index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        i++;
+    } 
+    ht->items[index] = item;
+    ht->count++;
+}
+
+char* ht_search(ht_hash_table* ht, const char* key) {
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (item != &HT_DELETED_ITEM) { 
+            if (strcmp(item->key, key) == 0) {
+                return item->value;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    } 
+    return NULL;
+}
+
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
+
+void ht_delete(ht_hash_table* ht, const char* key) {
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (item != &HT_DELETED_ITEM) {
+            if (strcmp(item->key, key) == 0) {
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    } 
+    ht->count--;
+}
+
+#pragma endregion
+
+#pragma region RESIZING
+static ht_hash_table* ht_new_sized(const int base_size) {
+    ht_hash_table* ht = xmalloc(sizeof(ht_hash_table));
+    ht->base_size = base_size;
+
+    ht->size = next_prime(ht->base_size);
+
+    ht->count = 0;
+    ht->items = xcalloc((size_t)ht->size, sizeof(ht_item*));
+    return ht;
+}
+
+
+ht_hash_table* ht_new() {
+    return ht_new_sized(0);
+}
+#pragma endregion
+int main() {
+    ht_hash_table* ht = ht_new();
+    ht_del_hash_table(ht);
+    return 0;
+}
